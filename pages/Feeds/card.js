5e000c1c276a1685592350bacff633140b6067fc
David-Mateus <david-mateus09@hotmail.com>
2023-03-21 00:01:23 -0300

    tela 3

--- pages/Feeds/card.js
+++ pages/Feeds/card.js
@@ -1,22 +1,10 @@
 import cardlist from "./data";
 import Image from "next/image";
-export default function Card() {
+import Link from "next/link";
+export default function Card({}) {
   return (
     <>
-    
-      <div>
-        <div className="grid lg:grid-cols-1">
-          {cardlist.map((card) => (
-            <Image
-            src={card.img}
-             width={180} 
-             height={180} 
-             alt="Logo"/>
-
-            // <p>{card.title}</p>
-          ))}
-        </div>
-      </div>
+      
     </>
   );
 }
